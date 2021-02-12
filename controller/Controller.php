3ab208ef2ff650e47f2fd6b83418cceabb0fbda8
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-02-12 11:20:01 +0100

    controller model inscription begin

--- controller/Controller.php
+++ controller/Controller.php
@@ -2,10 +2,22 @@
 
 namespace controller;
 
+use Exception;
+use model\UtilisateurModel;
+
 class Controller
 {
     public function inscription($donnees_u)
     {
-        var_dump($donnees_u);
+
+        // Verifier l'unicité du login, mail, téléphone
+        // Verifier que tous les champs sont renseignés
+        // Verifier que l'email correct
+        // Verifier que l'adresse correct 
+        // Verifier que les mots de pass sont identiques
+        if ($this->checkUserInscriptionData($donnees_u)) {
+            $utilisateurModel = new UtilisateurModel($donnees_u);
+            $utilisateurModel->add();
+        }
     }
 }
