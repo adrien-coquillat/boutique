e8096c3599a7564ed56b488083233b88d4f14d27
adrien-coquillat <adrien.coquillat@laplateforme.io>
2021-02-12 13:23:43 +0100

    controller

--- controller/Controller.php
+++ controller/Controller.php
@@ -11,13 +11,47 @@ class Controller
     {
 
         // Verifier l'unicité du login, mail, téléphone
-        // Verifier que tous les champs sont renseignés
-        // Verifier que l'email correct
+
         // Verifier que l'adresse correct 
-        // Verifier que les mots de pass sont identiques
+
         if ($this->checkUserInscriptionData($donnees_u)) {
             $utilisateurModel = new UtilisateurModel($donnees_u);
-            $utilisateurModel->add();
+            // $utilisateurModel->add();
         }
+    }
+
+    /**
+     * 
+     * 
+     */
+    public function checkUserInscriptionData($donnees_u)
+    {
+        $errorsmsg = [];
+
+        // foreach data of table, we check if they are not empty
+        foreach ($donnees_u as $key => $value) {
+            if (empty($value)) {
+                $errormsg[] = "Le champ $key doit être renseigné.";
+            }
+        }
+        // check if the mail is valid
+        if (!filter_var($donnees_u['mail_u'], FILTER_VALIDATE_EMAIL)) {
+            $errormsg[] = "L'adresse email {$donnees_u['mail_u']} est considérée comme invalide.";
+        }
+        // check if the password are the same 
+        if ($donnees_u['motdepass_u'] != $donnees_u['motdepass_u_conf']) {
+            $errormsg[] = "Les mots de passe ne correspondent pas.";
+        }
+        //check if the postal adress is correct
+        // <input type="text" name="nom_rue_adresse_u" placeholder="Nom de la rue">
+        // <input type="text" name="ville_adresse_u" placeholder="Ville">
+        // <input type="text" name="postal_adresse_u" placeholder="Code postal">
+        if (((int) $donnees_u['numero_rue_adresse_u']) == 0) {
+            $errormsg[] = "Le champ renseigné doit être un chiffre";
+        }
+
+        if (strlen($donnees_u['nom_rue_adresse_u']) > 1) {
+        }
+        var_dump($errormsg);
     }
 }
--- view/inscription.php
+++ view/inscription.php
@@ -3,7 +3,10 @@
     <input type="text" name="login_u" placeholder="Login">
     <input type="text" name="nom_u" placeholder="Nom">
     <input type="text" name="prenom_u" placeholder="Prénom">
-    <input type="text" name="adresse_u" placeholder="Adresse">
+    <input type="text" name="numero_rue_adresse_u" placeholder="N°">
+    <input type="text" name="nom_rue_adresse_u" placeholder="Nom de la rue">
+    <input type="text" name="ville_adresse_u" placeholder="Ville">
+    <input type="text" name="postal_adresse_u" placeholder="Code postal">
     <input type="email" name="mail_u" placeholder="Email">
     <input type="tel" name="telephone_u" placeholder="Téléphone">
     <input type="password" name="motdepass_u" placeholder="Password">
