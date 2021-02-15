8428072322f55396e1fe9075f25dddd540dc5416
adrien-coquillat <adrien.coquillat@laplateforme.io>
2021-02-15 14:43:52 +0100

    function connexion

--- controller/Controller.php
+++ controller/Controller.php
@@ -4,6 +4,7 @@ namespace controller;
 
 use entity\UtilisateurEntity;
 use Exception;
+use model\UtilisateurModel;
 
 class Controller
 {
@@ -14,14 +15,33 @@ class Controller
             return;
         }
 
-        $user = new UtilisateurEntity();
+        $userEntity = new UtilisateurEntity();
 
-        if (($msg = $user->checkData($donnees_u)) === TRUE) {
+        if (($msg = $userEntity->checkData($donnees_u)) === TRUE) {
             //$utilisateurModel = new UtilisateurModel($donnees_u);
             // $utilisateurModel->add();
         } else {
             throw new Exception(implode('<br />', $msg));
         }
         var_dump($msg);
+    }
+
+    public function connexion($donnee_u)
+    {
+        $userModel = new UtilisateurModel();
+
+        if ($userModel->connexion($donnee_u) == TRUE) {
+            header('Location: index.php?page=pannier');
+        } else {
+            header('Location: index.php?page=home&error=message');
+        }
+    }
+
+    public function home()
+    {
+        if (isset($_GET['error']) && ($_GET['error'] == 'message')) {
+            $msg = 'Les identifiants sont incorrects.';
+            throw new Exception($msg);
+        }
     }
 }
