6e82c1427145d042cf2906748151be40f9a0d330
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-03-04 16:31:54 +0100

    Carousel blur filter and text color implemented

--- view/home.php
+++ view/home.php
@@ -73,22 +73,22 @@
                 <div class="carousel-item active" data-bs-interval="5000">
                     <img src="public/img/<?= $produits[$conf['carousel_first_id_p']->value  - 1]->nom_image_p ?>" class="d-block img-custom-carousel" alt="...">
                     <div class="carousel-caption d-none d-md-block">
-                        <h5><?= $produits[$conf['carousel_first_id_p']->value  - 1]->nom_p ?></h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <h5 style="font-family: 'Robotto', sans-serif; font-size: 2.7rem; color: #FB4D94;"><?= $produits[$conf['carousel_first_id_p']->value  - 1]->nom_p ?></h5>
+                        <p style="font-family: 'Robotto', sans-serif; font-size: 1.5rem; color: #FB4D94;"><?= $produits[0]->troncateText($produits[$conf['carousel_first_id_p']->value  - 1]->description_p, 70) ?></p>
                     </div>
                 </div>
-                <div class="carousel-item" data-bs-interval="2000">
+                <div class="carousel-item" data-bs-interval="5000">
                     <img src="public/img/<?= $produits[$conf['carousel_second_id_p']->value  - 1]->nom_image_p ?>" class="d-block img-custom-carousel" alt=" ...">
                     <div class="carousel-caption d-none d-md-block">
-                        <h5><?= $produits[$conf['carousel_second_id_p']->value  - 1]->nom_p ?></h5>
-                        <p>Some representative placeholder content for the second slide.</p>
+                        <h5 style="font-family: 'Robotto', sans-serif; font-size: 2.7rem; color: #FB4D94;"><?= $produits[$conf['carousel_second_id_p']->value  - 1]->nom_p ?></h5>
+                        <p style="font-family: 'Robotto', sans-serif; font-size: 1.5rem; color: #FB4D94;"><?= $produits[0]->troncateText($produits[$conf['carousel_second_id_p']->value  - 1]->description_p, 70) ?></p>
                     </div>
                 </div>
-                <div class="carousel-item">
+                <div class="carousel-item" data-bs-interval="5000">
                     <img src="public/img/<?= $produits[$conf['carousel_third_id_p']->value  - 1]->nom_image_p ?>" class="d-block img-custom-carousel" alt="...">
                     <div class="carousel-caption d-none d-md-block">
-                        <h5><?= $produits[$conf['carousel_third_id_p']->value  - 1]->nom_p ?></h5>
-                        <p>Some representative placeholder content for the third slide.</p>
+                        <h5 style="font-family: 'Robotto', sans-serif; font-size: 2.7rem; color: #FB4D94;"><?= $produits[$conf['carousel_third_id_p']->value  - 1]->nom_p ?></h5>
+                        <p style="font-family: 'Robotto', sans-serif; font-size: 1.5rem; color: #FB4D94;"><?= $produits[0]->troncateText($produits[$conf['carousel_third_id_p']->value  - 1]->description_p, 70) ?></p>
                     </div>
                 </div>
             </div>
